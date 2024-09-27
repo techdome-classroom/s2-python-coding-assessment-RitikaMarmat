0e1bcd5ec13de7cf7b9f048674a66cb9a9e6c378
mahendrakr <mahendrakumar7028@gmail.com>
2024-09-27 13:59:23 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -5,6 +5,26 @@ class Solution(object):
         :rtype: int
         """
         pass
+        def romanToInt(s: str) -> int:
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    i = 0
+    
+    while i < len(s):
+        if i + 1 < len(s) and roman_values[s[i]] < roman_values[s[i + 1]]:
+            result += roman_values[s[i + 1]] - roman_values[s[i]]
+            i += 2
+        else:
+            result += roman_values[s[i]]
+            i += 1
+    
+    return result
+
+# Examples
+print(romanToInt("III"))    # Output: 3
+print(romanToInt("LVIII"))  # Output: 58
+print(romanToInt("MCMXCIV"))# Output: 1994
+
 
 
 
