7752ab081e9f2bd016e820a0069d2c95453ce0ba
mahendrakr <mahendrakumar7028@gmail.com>
2024-09-27 13:56:23 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,10 +1,5 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+    
         def isValid(s: str) -> bool:
     stack = []
     mapping = {')': '(', '}': '{', ']': '['}
