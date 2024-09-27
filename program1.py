756972243a12cb10cf3c9275d4d7ab218587b231
mahendrakr <mahendrakumar7028@gmail.com>
2024-09-27 14:06:34 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,25 +1,24 @@
 class Solution(object):
-    
-        def isValid(s: str) -> bool:
+   def is_valid_parentheses(s: str) -> bool:
     stack = []
-    mapping = {')': '(', '}': '{', ']': '['}
-
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
     for char in s:
-        if char in mapping:
-            top_element = stack.pop() if stack else '#'
-            if mapping[char] != top_element:
-                return False
-        else:
-            stack.append(char)
+        if char in bracket_map.values():
+            stack.append(char)  # If it's an opening bracket, push onto the stack
+        elif char in bracket_map.keys():
+            if not stack or stack[-1] != bracket_map[char]:
+                return False  # If it's a closing bracket and doesn't match the top of the stack
+            stack.pop()  # Pop the matching opening bracket from the stack
+            
+    return not stack  # If the stack is empty, all brackets are matched
 
-    return not stack
-
-# Examples
-print(isValid("()"))     
-print(isValid("()[]{}"))  
-print(isValid("(]"))      
-
-
+# Get user input
+user_input = input("Enter a string of parentheses: ")
+if is_valid_parentheses(user_input):
+    print("The string is valid.")
+else:
+    print("The string is invalid.")
 
 
 
